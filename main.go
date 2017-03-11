bbac79da1e5f8b9c4676b8b375054479d8e2d77c
aerth <aerth@riseup.net>
2017-03-11 11:16:55 -0800

    Adds support for things like 'gomaker ./cmd/example'

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func init() {
 	// redefine flag.Usage
 	usage := flag.Usage
 	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "[Gomaker] "+version+"\n")
+		fmt.Fprintln(os.Stderr, "[Gomaker "+version+"]\n")
 		fmt.Fprintln(os.Stderr, "Default:")
 		fmt.Fprintln(os.Stderr, "gomaker -o Makefile -options='static,verbose,lite,gitcommit' .")
 		fmt.Fprintln(os.Stderr, "gomaker -o Makefile -options='static,verbose,lite,gitcommit' ./cmd/name")
@@ -73,21 +73,20 @@ func main() {
 		} else {
 			flag.Usage()
 			fmt.Fprintf(os.Stderr, "error: need only one argument. got %v.\n", len(flag.Args()))
-
 		}
 		os.Exit(2)
 	}
 
 	// no args
-	args := flag.Arg(0)
+	projectDir := flag.Arg(0)
 
 	// make 'gomaker -v > Makefile' possible
-	if *verbose && args == "" {
-		args = "."
+	if *verbose && projectDir == "" {
+		projectDir = "."
 	}
 
 	// 'gomaker' or 'gomaker -o g,l,s,d' (no args)
-	if args == "" && !*verbose {
+	if projectDir == "" && !*verbose {
 		flag.Usage()
 		fmt.Fprintln(os.Stderr, "Fatal: need go main project directory as argument. Try 'gomaker .'")
 		os.Exit(2)
@@ -95,18 +94,19 @@ func main() {
 
 	// standard usage
 	// gomaker .
-	if args == "." {
+	var projectDirectory string // absolute
+
+	if projectDir == "." {
 		var err error
-		args, err = os.Getwd()
+		projectDirectory, err = os.Getwd()
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		args = strings.Replace(args, "//", "/", -1)
+		projectDirectory = strings.Replace(projectDir, "//", "/", -1)
 	}
 
-	projectDirectory := args
-	dir, err := ioutil.ReadDir(args)
+	dir, err := ioutil.ReadDir(projectDir)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Usage:")
 		flag.Usage()
@@ -128,11 +128,11 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Fatal: Not a Go project directory.")
 		os.Exit(2)
 	}
-	fmt.Fprintf(os.Stderr, "[Directory] %q", projectDirectory)
 
 	// Create the Makefile
-	fmt.Fprintln(os.Stderr, "[Gomaker] "+version)
-	fmt.Fprintln(os.Stderr, "[Makefile] "+*outputFile)
+	fmt.Fprintln(os.Stderr, "[Gomaker v"+version+"]")
+	fmt.Fprintf(os.Stderr, "[Makefile] %q\n",*outputFile)
+	fmt.Fprintf(os.Stderr, "[Project Dir] %q\n", projectDir)
 
 	var linein = make(chan string)
 	// Send things to linein
@@ -233,14 +233,15 @@ func writer(linein chan string) {
 
 func builder(linein chan string, dir string) {
 	// non-main package support soon come
-	fmt.Println("Checking", dir)
 	if !aMainPackage(dir) {
 		if !strings.Contains(strings.Join(os.Args, " "), "-f") {
 			os.Exit(2)
 		}
 	}
-	fmt.Println(dir, "is a main package")
 	// Since 'go build' uses the directory name as binary name, let's do the same.
+	if dir == "." {
+		dir, _ = os.Getwd()
+	}
 	dir = strings.TrimSuffix(dir, "/")
 	dirname := strings.Split(dir, "/")
 	projectName := dirname[len(dirname)-1]
@@ -380,7 +381,7 @@ func builder(linein chan string, dir string) {
 	echoe := Echo(fmt.Sprintf("Building ${NAME} version ${RELEASE}"))
 	linein <- "\t" + echoe
 	linein <- "\tgo get -d -x -v ."
-	linein <- "\tgo build -o ${NAME} " + fmt.Sprint(buildflags, ldflags, gcflags) + " " + dir
+	linein <- "\tgo build -o ${NAME} " + fmt.Sprint(buildflags, ldflags, gcflags) + " " + flag.Arg(0)
 	echoe = Echo(fmt.Sprintf("Successfully built ${NAME}"))
 	linein <- "\t" + echoe
 	linein <- "\n"
